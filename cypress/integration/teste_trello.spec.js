de1f0a4d61fa5f1dd56d9c65480b657834cee6e0
Tereza <terezaarual@hotmail.com>
2020-08-16 14:46:59 -0300

    Finalização da automação

--- cypress/integration/teste_trello.spec.js
+++ cypress/integration/teste_trello.spec.js
@@ -13,6 +13,30 @@ describe('Teste Trello',() =>{
         cy.get('button[class="button primary"]').click()
         cy.get('h1[class="js-board-editing-target board-header-btn-text"]').contains('Quadro de teste')
     })
-    
-   
-})
+
+    it('Validar criação de uma lista com nome To do', () =>{
+        cy.get('div[title="Quadro de teste"]').click()
+        cy.get('span[class="placeholder"]').click()
+        cy.get('input[class="list-name-input"]').type('To do')
+        cy.get('input[class="primary mod-list-add-button js-save-edit"]').click()
+        cy.get('h2[class="list-header-name-assist js-list-name-assist"]').contains('To do')
+    })
+
+    it('Validar criação do cartão com nome Atividade 1', () => {
+        cy.get('div[title="Quadro de teste"]').click()
+        var lista = cy.get('div[class="list js-list-content"]').contains('To do')
+        lista.get('span[class="js-add-a-card"]').click()
+        cy.get('textarea[class="list-card-composer-textarea js-card-title"]').type('Atividade 1')
+        cy.get('input[value="Adicionar Cartão"]').click()
+        cy.contains('Atividade 1')
+    })
+
+    it('Validar criação de um time', () => {
+        cy.get('div[title="Quadro de teste"]').click()
+        cy.get('span[class="board-header-btn-text"]').contains('Convidar').click()
+        cy.get('input[class="autocomplete-input"]').type('Tereza Almeida N')
+        cy.get('div[class="full-name"]').contains('Tereza Almeida Nobre de Sousa').click()
+        cy.get('[data-test-id="team-invite-submit-button"]').click()
+        cy.get('span[title="Tereza Almeida Nobre de Sousa (terezaalmeidanobredesousa)"]')
+     })
+}) 
